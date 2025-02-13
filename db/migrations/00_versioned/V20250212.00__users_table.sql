88e6be9d00c8a8bc1a81d93afe04d30f09838da4
curtbrink <crtbrnk+github@gmail.com>
2025-02-12 20:08:16 -0500

    17 - add typeorm packages; add user account db migration

---
+++ db/migrations/00_versioned/V20250212.00__users_table.sql
@@ -0,0 +1,16 @@
+create table user_account (
+    -- base schema
+    id uuid default gen_random_uuid() primary key,
+    created_at timestamptz default now(),
+    created_by uuid references user_account,
+    updated_at timestamptz default now(),
+    updated_by uuid references user_account,
+    deleted_at timestamptz,
+    deleted_by uuid references user_account,
+    data_version int default 1,
+    -- user schema
+    name varchar not null,
+    display_name varchar not null,
+    email varchar not null,
+    is_admin boolean not null default false
+);
---
+++ db/migrations/02_sample_data/V20250212.01__fresh_admin_user.sql
@@ -0,0 +1,4 @@
+insert into
+    user_account (name, display_name, email, is_admin)
+values
+    ('riktus', 'Riktus', 'crtbrnk@gmail.com', true);
